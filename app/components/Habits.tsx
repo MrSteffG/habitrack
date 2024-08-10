c4614502d9be28ba4aadb96246a956f4273ee506
Steff Gornall <steffgornall@gmail.com>
2024-08-10 17:19:08 +0100

    Improve styles ready too add functionality

--- app/components/Habits.tsx
+++ app/components/Habits.tsx
@@ -1,29 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-import strongData from "./strongData.json";
-
-const exampleDates = [
-  { day: "Mon", date: 5 },
-  { day: "Tue", date: 6 },
-  { day: "Wed", date: 7 },
-  { day: "Thu", date: 8 },
-  { day: "Fri", date: 9 },
-  { day: "Sat", date: 10 },
-  { day: "Sun", date: 11 },
-];
-
-const exampleHabits = ["Excercise", "Learn coding", "avoid chocolate"];
-const exampleSquares = [true, false, false, true, true, true, true];
-
-const habitCompletion = {};
-
-const showDates = exampleDates.map((dateObj) => (
-  <div className="flex h-10 w-10 flex-col" key={dateObj.date}>
-    <div>{dateObj.day}</div>
-    {dateObj.date}
-  </div>
-));
 
 const Habits = () => {
   const [habits, setHabits] = useState([
@@ -43,7 +20,6 @@ const Habits = () => {
 
   const [completed, setCompleted] = useState([
     {
-      habitID: 1,
       completionDay: "Mon",
       completionDate: "5",
       Excercise: true,
@@ -51,7 +27,6 @@ const Habits = () => {
       AvoidChocolate: true,
     },
     {
-      habitID: 1,
       completionDay: "Tue",
       completionDate: "6",
       Excercise: false,
@@ -59,7 +34,6 @@ const Habits = () => {
       AvoidChocolate: true,
     },
     {
-      habitID: 1,
       completionDay: "Wed",
       completionDate: "7",
       Excercise: false,
@@ -67,47 +41,90 @@ const Habits = () => {
       AvoidChocolate: true,
     },
     {
-      habitID: 1,
       completionDay: "Thu",
       completionDate: "8",
-      Excercise: false,
+      Excercise: true,
+      LearnCoding: true,
+      AvoidChocolate: true,
+    },
+    {
+      completionDay: "Fri",
+      completionDate: "9",
+      Excercise: true,
+      LearnCoding: true,
+      AvoidChocolate: true,
+    },
+    {
+      completionDay: "Sat",
+      completionDate: "10",
+      Excercise: true,
+      LearnCoding: true,
+      AvoidChocolate: true,
+    },
+    {
+      completionDay: "Sun",
+      completionDate: "11",
+      Excercise: true,
+      LearnCoding: true,
+      AvoidChocolate: true,
+    },
+    {
+      completionDay: "Mon",
+      completionDate: "12",
+      Excercise: true,
+      LearnCoding: true,
+      AvoidChocolate: true,
+    },
+    {
+      completionDay: "Tue",
+      completionDate: "13",
+      Excercise: true,
       LearnCoding: true,
       AvoidChocolate: true,
     },
   ]);
 
+  const showHabits = habits.map((habit) => (
+    <div className="flex h-10 items-center" key={habit.habitId}>
+      {habit.habit}
+    </div>
+  ));
+
   const showSquares = completed.map((item) => (
     <div className="flex flex-col gap-5" key={item.completionDate}>
-      <div className="flex h-10 w-10 flex-col" key={item.completionDate}>
+      <div
+        className="flex h-10 w-10 flex-col items-center"
+        key={item.completionDate}
+      >
         <div>{item.completionDay}</div>
         {item.completionDate}
       </div>
 
-      <div className="flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-1">
         {item.Excercise ? (
-          <div className="h-10 w-10 items-center justify-center bg-green-100 text-black">
-            {item.Excercise.toString()}
-          </div>
+          <div className="flex h-10 w-10 items-center justify-center rounded-sm bg-green-100 text-black hover:bg-red-50"></div>
         ) : (
-          <div className="h-10 w-10 items-center justify-center bg-red-100 text-black">
-            {item.Excercise.toString()}
-          </div>
+          <div className="flex h-10 w-10 items-center justify-center rounded-sm text-black hover:bg-green-50"></div>
+        )}
+        {item.LearnCoding ? (
+          <div className="flex h-10 w-10 items-center justify-center rounded-sm bg-green-100 text-black hover:bg-red-50"></div>
+        ) : (
+          <div className="flex h-10 w-10 items-center justify-center rounded-sm text-black hover:bg-green-50"></div>
+        )}
+        {item.AvoidChocolate ? (
+          <div className="flex h-10 w-10 items-center justify-center rounded-sm bg-green-100 text-black hover:bg-red-50"></div>
+        ) : (
+          <div className="flex h-10 w-10 items-center justify-center rounded-sm text-black hover:bg-green-50"></div>
         )}
       </div>
     </div>
   ));
 
-  const showHabits = habits.map((habit) => (
-    <div className="" key={habit.habitId}>
-      {habit.habit}
-    </div>
-  ));
-
   return (
-    <div className="flex w-2/3 rounded-lg bg-purple-400 bg-opacity-50 bg-gradient-to-r from-blue-400 p-3 font-semibold text-white">
-      <div className="to flex flex-col justify-evenly">{showHabits}</div>
+    <div className="flex w-1/3 rounded-lg bg-purple-400 bg-opacity-50 bg-gradient-to-r from-blue-400 p-3 font-semibold text-white">
+      <div className="flex flex-col justify-end gap-1">{showHabits}</div>
 
-      <div className="flex justify-evenly">{showSquares}</div>
+      <div className="flex gap-1 overflow-auto">{showSquares}</div>
     </div>
   );
 };
