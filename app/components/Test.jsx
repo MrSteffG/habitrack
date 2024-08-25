2f31bfed3fecabd4d672a6090214ce177070d396
Steff Gornall <steffgornall@gmail.com>
2024-08-25 12:30:16 +0100

    Squares toggle working

--- app/components/Test.jsx
+++ app/components/Test.jsx
@@ -2,10 +2,12 @@
 
 import React, { useEffect, useState } from "react";
 import dateArr, { datesSinceAugust } from "./dateArr";
-import completedArr, { newCompletedArr } from "./data";
+import { newCompletedArr } from "./data";
 
 const Test = () => {
   const dates = datesSinceAugust();
+
+  const [completed, setCompleted] = useState(newCompletedArr);
 
   const [habits, setHabits] = useState([
     {
@@ -33,7 +35,7 @@ const Test = () => {
   const showDates = dates.map((date) => (
     <div
       key={date}
-      className="flex h-10 w-10 flex-col items-center justify-center rounded-sm bg-slate-100 bg-opacity-20 text-sm"
+      className="flex h-10 w-10 flex-col items-center justify-center text-sm"
     >
       <div>{date.substring(0, 3)}</div>
       <div>{date.substring(8, 10)}</div>
@@ -43,14 +45,22 @@ const Test = () => {
   const showSquares = habits.map((habit) => (
     <div className="flex h-10 w-full items-center gap-1" key={habit.habitId}>
       {dates.map((date) => {
-        for (let i = 0; i < newCompletedArr.length; i++) {
+        for (let i = 0; i < completed.length; i++) {
           if (
-            (newCompletedArr[i].completionDay === date) &
-            (newCompletedArr[i].habitId === habit.habitId) &
-            newCompletedArr[i].done
+            (completed[i].completionDay === date) &
+            (completed[i].habitId === habit.habitId) &
+            completed[i].done
           ) {
             return (
-              <div className="flex h-10 w-10 items-center justify-center rounded-sm bg-green-200"></div>
+              <div
+                className="flex h-10 w-10 items-center justify-center rounded-sm bg-green-200"
+                onClick={() =>
+                  toggleCompletedOff({
+                    idToUpdate: completed[i].id,
+                    newData: !completed[i].done,
+                  })
+                }
+              ></div>
             );
           }
         }
@@ -58,14 +68,46 @@ const Test = () => {
           <div
             key={date}
             className="flex h-10 w-10 items-center justify-center rounded-sm bg-slate-100 bg-opacity-20"
+            onClick={() =>
+              toggleCompletedOn({
+                idToUpdate: Date.now(),
+                newData: true,
+                day: date,
+                habit: habit.habitId,
+              })
+            }
           ></div>
         );
       })}
     </div>
   ));
+
+  //toggles done value in completed array item to the opposite boolean
+  const toggleCompletedOff = ({ idToUpdate, newData }) => {
+    setCompleted(
+      completed.map(({ id, completionDay, habitId, done }) =>
+        id === idToUpdate
+          ? { id, done: newData, completionDay, habitId }
+          : { id, done, completionDay, habitId },
+      ),
+    );
+  };
+
+  const toggleCompletedOn = ({ idToUpdate, newData, day, habit }) => {
+    setCompleted([
+      ...completed,
+      {
+        id: idToUpdate,
+        done: newData,
+        completionDay: day,
+        habitId: habit,
+      },
+    ]);
+  };
+
   //Todo
-  //Get dateArr datatype and completed date datatype the same
-  //* To check completed map through the completed arr, if the dates match and then the habit id, and done is set to true, return a coloured square.
+  // Add toggle square functionality
+
   return (
     <div className="flex w-2/3 gap-5 rounded-lg bg-gradient-to-r from-blue-400 to-purple-300 p-3 font-semibold text-white">
       <div className="flex">
